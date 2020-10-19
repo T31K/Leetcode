f6db0e230acbd68961a6d3dccd960f5a4bdd4f7a
T31K <t31kmunwong@gmail.com>
2020-10-19 16:05:20 +0800

    update

--- subtractProductAndSum.js
+++ subtractProductAndSum.js
@@ -1,25 +1,18 @@
-
-
-
-
 const subtractProductAndSum = (n) => {
     let input = n.toString()
     let product = 1
     let sum = 0
-    let output = 0
+    
 
     for (let i = 0 ; i < input.length ; i++){
         product *= Number(input[i])
         sum += Number(input[i])
     }
 
-    return output = product + sum 
+    let output = product - sum
+    return output
 };
 
 
 
 
-
-
-// console.log(product)
-// console.log(sum)
