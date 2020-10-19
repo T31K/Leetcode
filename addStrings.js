e353b07475677bfe95afe0a22d2c1da020c99dd2
T31K <t31kmunwong@gmail.com>
2020-10-19 15:30:16 +0800

    balancedStringSplit

---
+++ addStrings.js
@@ -0,0 +1,13 @@
+// /**
+//  * @param {string} num1
+//  * @param {string} num2
+//  * @return {string}
+//  */
+const addStrings = (num1, num2) => {
+    return Number(num1) + Number(num2)
+};
+
+const num_1 = 5
+const num_2 = 5
+
+console.log(addStrings('5','5'))
