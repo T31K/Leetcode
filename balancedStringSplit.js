fbe55310f64e343635db3c869b5ab614e0f18e2b
T31K <t31kmunwong@gmail.com>
2020-10-19 15:48:19 +0800

    Update

--- balancedStringSplit.js
+++ balancedStringSplit.js
@@ -1,7 +1,17 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var balancedStringSplit = function(s) {
-    
+const balancedStringSplit = (s) => {
+    let r_counter = 0
+    let l_counter = 0
+    let total_output = 0
+    for (let i = 0 ; i < s.length ; i++){
+        if ( s[i] === 'R'){
+            return r_counter += 1
+        }
+        else if ( s[i] === 'L'){
+            return l_counter += 1
+        }
+        
+        if (r_counter === l_counter){
+            return total_output += 1
+        }
+    }
 };
