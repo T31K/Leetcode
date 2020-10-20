fb0570093452fc61c64be14efaa5197335928834
T31K <t31kmunwong@gmail.com>
2020-10-20 12:27:41 +0800

    Update

--- countGoodTriplets.js
+++ countGoodTriplets.js
@@ -1,1 +1,10 @@
-console.log('hello')
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+    
+};
