4285042aa049c9583fea2ea0baad4616f114b003
T31K <t31kmunwong@gmail.com>
2020-10-20 13:52:00 +0800

    Update

--- restoreString.js
+++ restoreString.js
@@ -1,12 +1,9 @@
-var s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+const restoreString = function(s, indices) {
+    let output = ''
+    for (let i = 0 ; i < indices.length ; i++){
+        output += (s[indices.indexOf(i)])
+        }
+    console.log(output)
+}
 
-
-// var restoreString = function(s, indices) {
-//     var input = indices.sort()
-//     var output = ""
-//     for (var i = 0 ; i < indices.length ; i++){
-//         output += s[indices[i]] 
-//         }
-//     return output
-// }
 console.log(restoreString(s, indices))
