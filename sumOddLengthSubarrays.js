db3d9bb313c96f803f0c753043984fbea78babbd
T31K <t31kmunwong@gmail.com>
2020-10-20 15:57:24 +0800

    Update

--- sumOddLengthSubarrays.js
+++ sumOddLengthSubarrays.js
@@ -5,30 +5,12 @@
 arr = [1,4,2,5,3]
 console.log(arr.length)
 var output = 0
+var len = 0
+// if even
+arr.length % 2 !== 0 ?
+len += (arr.length + 1 ) / 2 : len += arr.length
 
-    if (arr.length === 2){
-        for (var i = 0 ; i < arr.length ; i++){
-            output += arr[i]
-        }
-    }
-
-    else if (arr.length === 3){
-        for (var i = 0 ; i < arr.length ; i++){
-            output += arr[i] * 2
-        }
-    }
-
-    else {
-        if (arr.length % 2 !== 0){
-            var len = (arr.length + 1) / 2
-            for (var j = 0 ; j < len ; j++){
-                
-            }
-        }
-        else{
-
-        }
-    }
+console.log(len)
 
 
 // arr.length = 3
