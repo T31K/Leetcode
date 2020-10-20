4b1c1ae5cd45544e4649b0b59663a8b34d94b48e
T31K <t31kmunwong@gmail.com>
2020-10-20 15:38:18 +0800

    Update

--- decompressRLElist.js
+++ decompressRLElist.js
@@ -1,13 +1,9 @@
-const decompressRLElist = (nums) => {
-    let output = []
-    for (let i = 0 ; i < nums.length ; i++){
-        if (i % 2 === 0){
+var decompressRLElist = (nums) => {
+    const output = []
+    for (let i = 0 ; i < nums.length ; i += 2){
             for (let j = 0; j < nums[i] ; j++){
                 output.push(nums[i+1])
             }
-        } else {
-            continue
-        }
     }
     return output
 }
