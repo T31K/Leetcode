b510d8f375c50aed4b342ca83590c21b5410fe2a
T31K <t31kmunwong@gmail.com>
2020-10-20 13:09:10 +0800

    Update

--- findNumbers.js
+++ findNumbers.js
@@ -1,3 +1,26 @@
 const findNumbers = (nums) => {
-    console.log('hello')
-};
+    const string = nums.toString()
+    let counter = 0
+    let total = 0
+
+        for (let i = 0 ; i < string.length ; i++){
+            if (string[i] === ','){
+                if (counter % 2 == 0){
+                    total += 1
+                    counter = 0
+                } else {
+                    counter = 0
+                }
+                
+            } else {
+                counter += 1
+            }
+            
+            if (i === string.length - 1){
+                counter % 2 == 0 ? total += 1 : null
+            }
+        }
+
+    return total
+}
+
