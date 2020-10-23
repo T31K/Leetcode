7b2be1be87faa1f63c7b4f81c73625ca3335b9db
T31K <t31kmunwong@gmail.com>
2020-10-23 15:13:06 +0800

    update

--- sumOddLengthSubarrays.js
+++ sumOddLengthSubarrays.js
@@ -7,7 +7,7 @@ var i = arr.length
 var output = 0
 for (var i = arr.length ; i > 1 ; i -= 2){
     for (var j = 0 ; j < arr.length ; j++){
-        output += arr[j]
+        output += arr[j-arr.length]
     }
 }    
 
