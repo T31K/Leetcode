8001cfd48219b73259928ba2c07b6368eedb3d30
T31K <t31kmunwong@gmail.com>
2020-10-26 14:08:13 +0800

    Update

--- twoCitySchedCost.js
+++ twoCitySchedCost.js
@@ -1,26 +1,40 @@
 // twoCitySchedCost = function(costs) {
-    
+
 // };
-let output = []
+let output_ori = []
+let output_mod = []
+let a_counter = 0
+let b_counter = 0
 const costs = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
 for (var i = 0 ; i < costs.length ; i++){
-    for (var j = 0 ; j < costs[i].length ; j++){
-        output.push(costs[i][j])
-    }
+    var diff = costs[i][1] - costs[i][0]
+    output_ori.push(diff)
+    output_mod.push(Math.abs(diff))
 }
+// console.log(output_ori)
+// console.log(output_mod)
+for (var j = 0 ; j < output_mod.length ; j++){
+    Math.mod.apply(0,output_mod)
+}
+console.log(output_mod.indexOf(Math.min.apply(0, output
 
-console.log(output)
+// const min = Math.min.apply(0, output)
+// output[output.indexOf(Math.min.apply(0, output))] = max
+// console.log(output)
 
+// console.log(output.indexOf(max))
+// // console.log(output)
+// // console.log(output.indexOf(Math.min.apply(0, output)))
 
-// 259 770
-// 448 54 B
-// 926 667 B
-// 184 139
-// 840 118 B
-// 577 469 
+// 259 770 2 A
+// 448 54  3 B
+// 926 667 4 B
+// 184 139 6
+// 840 118 1 B
+// 577 469 5
 
-//  index %2 === 0 A
-//   index %2 !== 0 B
+//   index %2 === 0         => A +1
+//   index %2 !== 0         => B -1
 
 
 
