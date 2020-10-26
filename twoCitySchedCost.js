cc66c73e1eee72a5ef7620707b3789656783bd3d
T31K <t31kmunwong@gmail.com>
2020-10-26 13:03:06 +0800

    Update

--- twoCitySchedCost.js
+++ twoCitySchedCost.js
@@ -1,3 +1,27 @@
-twoCitySchedCost = function(costs) {
+// twoCitySchedCost = function(costs) {
     
-};
+// };
+let output = []
+const costs = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
+for (var i = 0 ; i < costs.length ; i++){
+    for (var j = 0 ; j < costs[i].length ; j++){
+        output.push(costs[i][j])
+    }
+}
+
+console.log(output)
+
+
+// 259 770
+// 448 54 B
+// 926 667 B
+// 184 139
+// 840 118 B
+// 577 469 
+
+//  index %2 === 0 A
+//   index %2 !== 0 B
+
+
+
+
