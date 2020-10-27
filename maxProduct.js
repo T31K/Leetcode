6a0e84503a8b9cadc35ac07f00fc9c08111bc90e
T31K <t31kmunwong@gmail.com>
2020-10-27 12:51:15 +0800

    Update

--- maxProduct.js
+++ maxProduct.js
@@ -1,28 +1,8 @@
+var maxProduct = function(nums) {
+    let first_max = Math.max(...nums)
+    let index = nums.indexOf(first_max)
+    nums.splice(index,1)
+    let second_max = Math.max(...nums)
 
-// var maxProduct = function(nums) {
-    
-// };
-
-const nums = [3,4,5,2]
-var max = Math.max.apply(0,nums)
-let counter = 0
-let output = 0
-for (var i = 0 ; i < nums.length ; i++){
-    if (nums[i] === max){
-        counter += 1
-        
-    } else {
-        continue
-    }
-}
-let final = counter > 1 ? true : false
-
-if (final){
-    output += (max - 1) * (max - 1)
-} else {
-    const result = nums.filter(num => num < max)
-    var new_max = Math.max.apply(0,result)
-    output += (max - 1) * (new_max - 1)
-}
-
-console.log(output)
+    return (first_max - 1) * (second_max - 1)
+};
