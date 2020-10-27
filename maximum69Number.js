0cae5d5b8b38dc8bfd96b52cfbb123d440b0cbc3
T31K <t31kmunwong@gmail.com>
2020-10-27 12:27:45 +0800

    Update

--- maximum69Number.js
+++ maximum69Number.js
@@ -1,4 +1,21 @@
 
 var maximum69Number  = function(num) {
-    
-};
+
+let str = num.toString()
+let output = ""
+let six_counter = 0
+for (var i = 0 ; i < str.length ; i++){
+    if (str[i] === '6'){
+        if (six_counter === 0){
+        output += "9"
+        six_counter += 1
+        } else {
+            output += "6"
+        }
+    }  else {
+        output += "9"
+    }
+}
+
+return(Number(output))  
+}
