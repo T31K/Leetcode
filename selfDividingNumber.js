99d01e43cc072accf12d767dc7a0629b9822f634
T31K <t31kmunwong@gmail.com>
2020-11-02 10:35:47 +0800

    Update

--- selfDividingNumber.js
+++ selfDividingNumber.js
@@ -1,28 +1,20 @@
+var selfDividingNumbers = function(left, right) {
+    let i = left;
+    var output = []
 
-// var selfDividingNumbers = function(left, right) {
-    
-// };
-const left = 1;
-let i = left;
-const right = 22;
-var output = []
-
-while (i <= right){
-    if (i < 10) {
-        output.push(i)
-    } else{
-        let str = i.toString()
-        let final = 0
-        for (var j = 0 ; j < str.length ; j++){
-            i % Number(str[j]) === 0 ?
-            null : final -= 1
-        }
-        final === 0 ? output.push(i) : null
-    }  
-    i++
+    while (i <= right){
+        if (i < 10) {
+            output.push(i)
+        } else{
+            let str = i.toString()
+            let final = 0
+            for (var j = 0 ; j < str.length ; j++){
+                i % Number(str[j]) === 0 ?
+                null : final -= 1
+            }
+            final === 0 ? output.push(i) : null
+        }  
+        i++
+    }
+    return output
 }
-
-
-
-
-console.log(output)
