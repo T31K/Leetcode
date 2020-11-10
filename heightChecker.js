e79f15fcd29b96196ffa37fea4bbdd0da05ef738
T31K <t31kmunwong@gmail.com>
2020-11-10 17:56:29 +0800

    Added heighChecker (easy)

--- heightChecker.js
+++ heightChecker.js
@@ -1,6 +1,18 @@
-/**
- * @param {number[]} heights
- * @return {number}
- */
-var heightChecker = function(heights) {
-    
+
+// var heightChecker = function(heights) {
+
+const heights = [10,6,6,10,10,9,8,8,3,3,8,2,1,5,1,9,5,2,7,4,7,7]; 
+let output = 0
+let answer = []
+for ( var j = 0 ; j < heights.length ; j++){
+    answer.push(heights[j])
+}
+answer.sort(function(a, b){return a-b});
+// console.log(answer)
+for (var i = 0 ; i < heights.length ; i++){
+    if (answer[i] !== heights[i]){
+        output++
+    }
+}
+console.log(output)
+
