7dcdc40b343f2bb23c97db5949ab768b2c381361
T31K <t31kmunwong@gmail.com>
2020-11-15 14:04:14 +0800

    Update

--- invalidTransactions.js
+++ invalidTransactions.js
@@ -1,26 +1,41 @@
 
+const transactions = ["alex,676,260,bangkok","bob,656,1366,bangkok","alex,393,616,bangkok","bob,820,990,amsterdam","alex,596,1390,amsterdam"]
 // var invalidTransactions = function(transactions) {
-    
-// };
-let output = []
-let str = ""
-const transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
-for (var i = 0 ; i < transactions.length ; i++){
-    for (var j = 0 ; j < transactions[i].length ; j++){
-        if (transactions[i][j] !== ","){
-            if (j !== transactions[i].length - 1){
-                str += transactions[i][j]
+    let output = []
+    let str = ""
+    let final = []
+    for (var i = 0 ; i < transactions.length ; i++){
+        for (var j = 0 ; j < transactions[i].length ; j++){
+            if (transactions[i][j] !== ","){
+                if (j !== transactions[i].length - 1){
+                    str += transactions[i][j]
+                } else {
+                    str += transactions[i][j]
+                    output.push(str)
+                    str = ""
+                }
             } else {
-                str += transactions[i][j]
                 output.push(str)
                 str = ""
             }
+        }        
+    }
+    if (Number(output[2]) < 1000){
+        if(Number(output[6]) < 1000){
+            if (output[0] === output[4]){
+                if (output[3] !== output[7]){
+                    if (Number(output[5]) - Number(output[1]) < 60){
+                        final.push(transactions[1])
+                        final.push(transactions[0])
+                    }
+                }
+            }
         } else {
-            output.push(str)
-            str = ""
+            final.push(transactions[1])
         }
-        
-    }        
-
-}
+    } else {
+        final.push(transactions[0])
+    };
+    // return final
+// }
 console.log(output)
