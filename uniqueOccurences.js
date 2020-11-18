9af51457790bc20696fed4527f6847056a5ad85a
T31K <t31kmunwong@gmail.com>
2020-11-18 09:39:25 +0800

    Solved uniqueOccurences.js (easy)

--- uniqueOccurences.js
+++ uniqueOccurences.js
@@ -1,4 +1,17 @@
 
 var uniqueOccurrences = function(arr) {
-    
-};
+    let output = []
+    let unique = [...new Set(arr)];
+
+    for (var i = 0 ; i < unique.length ; i ++){
+        let result = arr.filter(arr => arr === unique[i])
+        output.push(result.length)
+    }
+
+    let final = [...new Set(output)];
+    if (final.toString() === output.toString()){
+        return true
+    } else {
+        return false
+    }
+}
