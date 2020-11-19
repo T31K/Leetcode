85fc1de648f465c1e1c3640a17290c348ebb2f2b
T31K <t31kmunwong@gmail.com>
2020-11-19 11:06:59 +0800

    Completed reverseWords (easy)

--- reverseWords.js
+++ reverseWords.js
@@ -1,7 +1,26 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var reverseWords = function(s) {
+const s = "Let's take LeetCode contest"
+
+// var reverseWords = function(s) {
     
-};
+// };
+let arr = []
+let str = ""
+let output = ""
+for (var i = 0 ; i < s.length + 1 ; i++){
+    if (s[i] !== " " && i !== s.length){
+        str += s[i]
+    } else {
+        arr.push(str)
+        str = ""
+    }
+}
+
+for (var j = 0 ; j < arr.length ; j++){
+    for (var k = arr[j].length - 1 ; k >= 0 ; k--){
+        output += arr[j][k]
+    } 
+    if (j !== arr.length - 1){
+        output += " "
+
+    }
+}
