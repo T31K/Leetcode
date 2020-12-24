f92f6a1fa3b2f8410de8f3b8071244db6d392761
T31K <t31kmunwong@gmail.com>
2020-12-24 17:11:58 +0800

    Solved containsDuplicate.js (easy) - 80ms/46mb > 94%

---
+++ containsDuplicate.js
@@ -0,0 +1,30 @@
+// Runtime: 80 ms, faster than 94.10% of JavaScript online submissions for Contains Duplicate.
+// Memory Usage: 41.6 MB, less than 84.78% of JavaScript online submissions for Contains Duplicate.
+// APPROACH
+// 1. SORT
+// 2. CHECK IF I === I+1
+// 3. IF SAME RETURN TRUE AND BREAK
+// 4. IF NOT CONITNUE CHECKING
+// 5. IF END OF THE ARRAY MEANS THERE IS NO DUPLICATE, RETURN FALSE
+
+var containsDuplicate = function(input) {
+    let sorted = input.sort( (a,b) => a-b)
+    if (sorted.length > 1){
+        for (var i = 0 ; i < sorted.length ; i++){
+            if (sorted[i] === sorted[i+1]){
+                return true
+                break
+            } else {
+                if (i !== sorted.length - 1){
+                    continue
+                } else {
+                    return false
+                }
+            }
+        }
+    } else {
+       return false
+    }
+    
+    
+    };
