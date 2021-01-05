7c0b345fcba13908627de3ca7974c395a28dd08b
T31K <t31kmunwong@gmail.com>
2021-01-05 09:39:59 +0800

    Solved arrayStringsAreEqual - 68ms/38mb >98%

---
+++ arrayStringsAreEqual.js
@@ -0,0 +1,6 @@
+// Runtime: 68 ms, faster than 98.65% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
+// Memory Usage: 38.7 MB, less than 62.74% of JavaScript online submissions for Check If Two String Arrays are Equivalent.
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join("") === word2.join("")
+};
