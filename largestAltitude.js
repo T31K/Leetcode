05b25cccd1ed458f52045ddc549c6fa9c1123c79
T31K <t31kmunwong@gmail.com>
2021-03-07 21:45:31 +0800

    Solved largestAltitude

---
+++ largestAltitude.js
@@ -0,0 +1,16 @@
+// Runtime: 72 ms, faster than 92.23% of JavaScript online submissions for Find the Highest Altitude.
+// Memory Usage: 38.3 MB, less than 81.66% of JavaScript online submissions for Find the Highest Altitude.
+
+let checker = 0
+let max = 0
+for (let i = 0 ; i < gain.length ; i++) {
+    checker += gain[i]
+    if (checker > max){
+        max = checker
+    }
+}
+
+const largestAltitude = (gain) {
+    
+};
+console.log(max)
