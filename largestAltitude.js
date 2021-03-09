1fa54dbb9bd75407f40b5789635e56ef6743976f
T31K <t31kmunwong@gmail.com>
2021-03-09 15:51:08 +0800

    Update

--- largestAltitude.js
+++ largestAltitude.js
@@ -1,16 +1,14 @@
 // Runtime: 72 ms, faster than 92.23% of JavaScript online submissions for Find the Highest Altitude.
 // Memory Usage: 38.3 MB, less than 81.66% of JavaScript online submissions for Find the Highest Altitude.
 
-let checker = 0
-let max = 0
-for (let i = 0 ; i < gain.length ; i++) {
-    checker += gain[i]
-    if (checker > max){
-        max = checker
+const largestAltitude = (gain) => {
+    let checker = 0
+    let max = 0
+    for (let i = 0 ; i < gain.length ; i++) {
+        checker += gain[i]
+        if (checker > max){
+            max = checker
+        }
     }
+    return max
 }
-
-const largestAltitude = (gain) {
-    
-};
-console.log(max)
