0ec777eff2c80eaf1e7de61b8bca41f38cfbac22
T31K <t31kmunwong@gmail.com>
2021-03-25 14:08:30 +0800

    Solved #1470 shuffleArray

--- shuffleArray.js
+++ shuffleArray.js
@@ -1,15 +1,15 @@
-var shuffle = function(nums, n) {
-    
-    var i = 0
-    var ii = n * 2
-    newList = []
-    
-    while (i < nums.length / 2){
-        newList.push(nums[i])
-        newList.push(nums[n])
-        i++
-        n++
-    }
-    
-    return newList
-};
+// Runtime: 88 ms, faster than 68.48% of JavaScript online submissions for Shuffle the Array.
+// Memory Usage: 40.5 MB, less than 56.50% of JavaScript online submissions for Shuffle the Array.
+
+const shuffleArray = (nums, n) => {
+  let counter = n + 0
+  let output = []
+  for (let i = 0; i < counter; i++) {
+    output.push(nums[i])
+    output.push(nums[n])
+    n++
+  }
+  return output
+}
+
+shuffleArray()
