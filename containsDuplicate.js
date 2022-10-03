dd7b04f2b424d7744f7e94ddd9258a7510b1435d
T31K <t31kmunwong@gmail.com>
2022-10-03 09:35:12 +0800

    #217 contains duplicate

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -7,24 +7,33 @@
 // 4. IF NOT CONITNUE CHECKING
 // 5. IF END OF THE ARRAY MEANS THERE IS NO DUPLICATE, RETURN FALSE
 
-var containsDuplicate = function(input) {
-    let sorted = input.sort( (a,b) => a-b)
-    if (sorted.length > 1){
-        for (var i = 0 ; i < sorted.length ; i++){
-            if (sorted[i] === sorted[i+1]){
-                return true
-                break
-            } else {
-                if (i !== sorted.length - 1){
-                    continue
-                } else {
-                    return false
-                }
-            }
+// 24/12/2020
+var containsDuplicate = function (input) {
+  let sorted = input.sort((a, b) => a - b);
+  if (sorted.length > 1) {
+    for (var i = 0; i < sorted.length; i++) {
+      if (sorted[i] === sorted[i + 1]) {
+        return true;
+        break;
+      } else {
+        if (i !== sorted.length - 1) {
+          continue;
+        } else {
+          return false;
         }
-    } else {
-       return false
+      }
     }
-    
-    
-    };
+  } else {
+    return false;
+  }
+};
+
+// 03/10/2022
+const containsDuplicate = function (nums) {
+  if (nums.length === 1) {
+    return false;
+  } else {
+    let unique = [...new Set(nums)];
+    return unique.toString() !== nums.toString();
+  }
+};
