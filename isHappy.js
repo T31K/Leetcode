03ffe3ee86bf8d2b1fdbdcf25d20e309906e396a
T31K <t31kmunwong@gmail.com>
2022-11-11 14:09:29 +0800

    #202 happy number

--- isHappy.js
+++ isHappy.js
@@ -1,9 +1,25 @@
-let isHappy = (n) => {
-  while (n >= 10) {
-    let m = n.toString()
-    let number = Math.pow(Number(m[0]), 2) + Math.pow(Number(m[1]), 2)
-    n = number + 0
-  }
+function getNum(num) {
+  let total = 0;
 
-  return n === 1
+  while (num !== 0) {
+    let lastDigit = num % 10;
+    num = Math.floor(num / 10);
+    total += lastDigit * lastDigit;
+  }
+  return total;
 }
+
+var isHappy = function (n) {
+  let fast = n;
+  let slow = n;
+
+  while (true) {
+    fast = getNum(getNum(fast));
+    slow = getNum(slow);
+
+    if (fast === slow) {
+      return fast === 1;
+      break;
+    }
+  }
+};
